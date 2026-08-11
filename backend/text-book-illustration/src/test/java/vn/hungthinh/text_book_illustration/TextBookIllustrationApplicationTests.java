30feff3f3ba6d4dcd9515d2a16bfa88437a25f65
hthinh24 <hungthinh240305@gmail.com>
2026-08-11 17:22:48 +0700

    Init Backend Skeleton

---
+++ backend/text-book-illustration/src/test/java/vn/hungthinh/text_book_illustration/TextBookIllustrationApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.hungthinh.text_book_illustration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextBookIllustrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
