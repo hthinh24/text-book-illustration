558c6757a6ce3f595b598128f65f6fe8a6b51aa0
hthinh24 <hungthinh240305@gmail.com>
2026-08-13 20:13:25 +0700

    chore: update start.sh allow run both FE & BE at the same time

--- start.sh
+++ start.sh
@@ -1,16 +1,24 @@
+#!/bin/bash
 set -a
 source .env
 set +a
 
 docker-compose up -d
 
-cd backend/text-book-illustration
-./mvnw spring-boot:run
+cleanup() {
+  echo "Stopping..."
+  kill "$BACKEND_PID" "$FRONTEND_PID" 2>/dev/null
+  exit
+}
+trap cleanup SIGINT SIGTERM
 
-#cd backend
-#cd backend/text-book-illustration
-#./mvnw spring-boot:run &
-#
-#cd ../frontend
-#npm install
-#npm run dev
+cd backend/text-book-illustration
+./mvnw spring-boot:run &
+BACKEND_PID=$!
+
+cd ../../frontend/text-book-illustration
+npm install
+npm run dev &
+FRONTEND_PID=$!
+
+wait "$BACKEND_PID" "$FRONTEND_PID"
